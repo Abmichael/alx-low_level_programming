0685bc165a5f91d0933993114ffa5678337c221f
Abraham Michael <abrahammgag@gmail.com>
2023-02-23 19:17:27 +0300

    done all tasks incl. optionals

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: int.
+ */
+
+int main(void)
+{
+	long n, i;
+
+	n = 612852475143;
+	for (i = 2; i < n; i++)
+	{
+		while (n % i == 0)
+			n = n / i;
+	}
+
+	printf("%lu\n", n);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul -multiplies two numbers
+ *@a: int
+ *@b: int
+ * Return: int.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
